0e6e3b19707d98afdf905be842a4ae3c33706eb5
lg <leonardoagustingimenez@gmail.com>
2023-09-13 11:07:03 -0300

    Listo?

--- js/space.js
+++ js/space.js
@@ -2,43 +2,47 @@ const inputBuscar = document.getElementById('inputBuscar');
 const btnBuscar = document.getElementById('btnBuscar');
 const contenedor = document.getElementById('contenedor');
 
-/*btnBuscar.addEventListener("click", () => {
+function mostrarResultados(data) {
+    contenedor.innerHTML = ''; //Esta línea borra cualquier contenido previamente presente en el elemento HTML con el id "contenedor
 
-    const response = fetch(`https://images-api.nasa.gov/search?q=${inputBuscar.value}`)
-        .then(response => response.json())
-        .then(data => {
-            data.collection.items.forEach(element => {
-                contenedor.innerHTML += `
-                    <div class="container" >
-                        <div class="row">
-                            <div class="col-md-4">
-                                <div class="card mb-4 shadow-sm custom-card cursor-active" id="autos">
-                                    <h3 class="m-3">Autos</h3>
-                                    <div class="card-body">
-                                        <p class="card-text">Los mejores precios en autos 0 kilómetro, de alta y media gama.</p>
-                                    </div>
-                                    <img class="bd-placeholder-img card-img-top" src="${element.links[0].href.value}" alt="Imgagen representativa de la categoría 'Autos'">
-                                </div>
+    const items = data.collection.items || []; // Esta línea crea una variable llamada items que obtiene el valor de data.collection.items. data es un objeto que se pasa como argumento a la función mostrarResultados. La expresión data.collection.items intenta acceder a una propiedad llamada "items" dentro del objeto "collection" que está dentro del objeto "data". Si esta propiedad no existe, o si data.collection o data son nulos o indefinidos, se asigna un array vacío a items. Esto se hace para evitar errores si los datos no están en el formato esperado.
+
+    if (items.length === 0) {  //  Aquí se verifica si la longitud del array items es igual a cero, lo que significa que no se encontraron imágenes en los datos proporcionados. Si esto es cierto, el contenido del elemento HTML con el id "contenedor" se establece como el texto "No se encontraron imágenes."
+        contenedor.textContent = 'No se encontraron imágenes.';
+    } else {
+        //contenedor.innerHTML += '<div class="row row-cols-3 row-cols-md-3 g-4">'
+        items.forEach(item => {
+            const title = item.data[0].title;
+            const description = item.data[0].description;
+            const imgagenURL = item.links[0].href;
+            contenedor.innerHTML += `
+                    <div class="col">
+                        <div class="card h-50">
+                            <img src="${imgagenURL}" class="card-img-top" alt="...">
+                            <div class="card-body mt-2">
+                                <h5 class="card-title"><strong>${title}</strong></h5>
+                                <p class="card-text">${description}</p>
                             </div>
                         </div>
                     </div>
-                `
-            })
-        });
-});*/
-
-btnBuscar.addEventListener('click', () => {
-    const buscar = inputBuscar.value;
-    const Url = `https://images-api.nasa.gov/search?q=${buscar}`;
-    
-    fetch(Url)
-        .then(response => response.json())
-        .then(data => {
-            mostrarResultados(data);
+            `
         })
-        .catch(error => {
-            console.error('Error al realizar la solicitud:', error);
-        });
+        //contenedor.innerHTML += '</div>'
+    }
+}
 
-});
+    btnBuscar.addEventListener('click', () => {
+        const buscar = inputBuscar.value;
+        const Url = `https://images-api.nasa.gov/search?q=${buscar}`;
+
+        fetch(Url)
+            .then(response => response.json())
+            .then(data => {
+                mostrarResultados(data);
+            })
+            .catch(error => {
+                console.error('Error al realizar la solicitud:', error);
+            });
+
+    });
 
